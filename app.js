72e2870a8d44ac72c49ca8527647d27e277d5d7a
saulo <sauloassis4123@gmail.com>
2023-10-06 16:03:14 -0300

    feat: Adding Global Error handling middleware

--- app.js
+++ app.js
@@ -30,9 +30,24 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Cannot find (${req.originalUrl})on this Server!!!`,
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Cannot find (${req.originalUrl})on this Server!!!`,
+  // });
+  const err = new Error(`Cannot find (${req.originalUrl})on this Server!!!`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 5000;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 // START SERVER
