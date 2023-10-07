417090fcecc8f2df8c072b42c1553b247318d449
saulo <sauloassis4123@gmail.com>
2023-10-07 00:28:50 -0300

    feat:better  refactor error like adding catch error in async function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
+
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -30,26 +33,22 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
+  // 1ª tratamento de erro
   // res.status(404).json({
   //   status: "fail",
   //   message: `Cannot find (${req.originalUrl})on this Server!!!`,
   // });
-  const err = new Error(`Cannot find (${req.originalUrl})on this Server!!!`);
-  err.status = "fail";
-  err.statusCode = 404;
+  // 2º mockando  erro
+  // const err = new Error(`Cannot find (${req.originalUrl})on this Server!!!`);
+  // err.status = "fail";
+  // err.statusCode = 404;
 
-  next(err);
+  // 3 tratamento de erro
+
+  next(new AppError(`Cannot find (${req.originalUrl})on this Server!!!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 5000;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 // START SERVER
 
 module.exports = app;
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require("./../models/tourModel");
 const APIFeatures = require("./../utils/apiFeatures");
+const catchAsync = require("./../utils/catchAsync");
 
 exports.aliasTopTours = async (req, res, next) => {
   req.query.limit = "5";
@@ -8,214 +9,183 @@ exports.aliasTopTours = async (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
+exports.getAllTours = catchAsync(async (req, res, next) => {
   // http://localhost:3000/api/v1/tours?duration[gte]=5 router to use in insominia for lt|gt|lte|gte
-  try {
-    // GETALL
-    // const query = Tour.find({});  getAllTours
 
-    // BUILD QUERY
-    //filtering
-    // const queryObj = { ...req.query };
-    // const excludeFields = ["page", "sort", "limit", "fields"];
+  // GETALL
+  // const query = Tour.find({});  getAllTours
 
-    // excludeFields.forEach((el) => delete queryObj[el]);
+  // BUILD QUERY
+  //filtering
+  // const queryObj = { ...req.query };
+  // const excludeFields = ["page", "sort", "limit", "fields"];
 
-    // const query = Tour.find({
-    //   duration: 5,
-    //   // difficulty: "easy", precisa deletar tudo e melhorar o model tour para depois importar todos dados novamente
-    // });
-    // const query = Tour.find({
-    //   duration: { $lte: 5 },
-    // });
+  // excludeFields.forEach((el) => delete queryObj[el]);
 
-    // const tours = await Tour.find({}).where("duration").equals(5);
-    // const tours = await Tour.find({}).where("duration").lte(5);
+  // const query = Tour.find({
+  //   duration: 5,
+  //   // difficulty: "easy", precisa deletar tudo e melhorar o model tour para depois importar todos dados novamente
+  // });
+  // const query = Tour.find({
+  //   duration: { $lte: 5 },
+  // });
 
-    // 2 advanc filtering (using regular expression)
+  // const tours = await Tour.find({}).where("duration").equals(5);
+  // const tours = await Tour.find({}).where("duration").lte(5);
 
-    // let queryStr = JSON.stringify(queryObj);
-    // queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
-    // // console.log(JSON.parse(queryStr), queryStr);
+  // 2 advanc filtering (using regular expression)
 
-    // let query = Tour.find(JSON.parse(queryStr));
+  // let queryStr = JSON.stringify(queryObj);
+  // queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
+  // // console.log(JSON.parse(queryStr), queryStr);
 
-    // FIELD SORTING
-    // console.log(req.query);
-    // if (req.query.sort) {
-    //   const sortBy = req.query.sort.split(",").join(" ");
-    //   console.log(sortBy);
-    //   query = query.sort(sortBy);
-    // } else {
-    //   query = query.sort("ratingsAverage");
-    // }
+  // let query = Tour.find(JSON.parse(queryStr));
 
-    // FIELD LIMITING
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(",").join(" ");
-    //   console.log(fields);
-    //   query = query.select(fields);
-    // } else {
-    //   query = query.select("-__v");
-    // }
+  // FIELD SORTING
+  // console.log(req.query);
+  // if (req.query.sort) {
+  //   const sortBy = req.query.sort.split(",").join(" ");
+  //   console.log(sortBy);
+  //   query = query.sort(sortBy);
+  // } else {
+  //   query = query.sort("ratingsAverage");
+  // }
 
-    // PAGINATION
-    // const page = req.query.page * 1 || 1;
-    // const limit = req.query.limit * 1 || 100;
-    // const skip = (page - 1) * limit;
-    // // para page 3 e limit 10 => ... 10-20 ;[20 -30] 30-40; ....
-    // query = query.skip(skip).limit(limit);
+  // FIELD LIMITING
+  // if (req.query.fields) {
+  //   const fields = req.query.fields.split(",").join(" ");
+  //   console.log(fields);
+  //   query = query.select(fields);
+  // } else {
+  //   query = query.select("-__v");
+  // }
 
-    // if (req.query.page) {
-    //   const numDocuments = await Tour.countDocuments();
-    //   if (skip >= numDocuments) throw new Error("This page doesn't exist");
-    // }
+  // PAGINATION
+  // const page = req.query.page * 1 || 1;
+  // const limit = req.query.limit * 1 || 100;
+  // const skip = (page - 1) * limit;
+  // // para page 3 e limit 10 => ... 10-20 ;[20 -30] 30-40; ....
+  // query = query.skip(skip).limit(limit);
 
-    // EXECQUERY
-    const features = new APIFeatures(Tour.find(), req.query)
-      .filter()
-      .sort()
-      .limiting()
-      .pagination();
-    // const tours = await query;
-    const tours = await features.query;
+  // if (req.query.page) {
+  //   const numDocuments = await Tour.countDocuments();
+  //   if (skip >= numDocuments) throw new Error("This page doesn't exist");
+  // }
 
-    // SEND RESPONSE
+  // EXECQUERY
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limiting()
+    .pagination();
+  // const tours = await query;
+  const tours = await features.query;
 
-    res.status(200).json({
-      status: "success",
-      results: tours.length,
+  // SEND RESPONSE
 
-      data: {
-        tours: tours,
-      },
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e.message,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
 
-exports.getTour = async (req, res) => {
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
   const id = await req.params.id;
   console.log(id);
 
-  try {
-    //const tour = await Tour.findOne({ _id: id });
-    const tour = await Tour.findById(id);
-    res.status(200).json({
-      status: "success",
+  //const tour = await Tour.findOne({ _id: id });
+  const tour = await Tour.findById(id);
+  res.status(200).json({
+    status: "success",
 
-      data: {
-        tours: tour,
-      },
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e.message,
-    });
-  }
-};
+    data: {
+      tours: tour,
+    },
+  });
+});
 
-exports.createTour = async (req, res) => {
+exports.createTour = catchAsync(async (req, res, next) => {
   // const newTour = new Tour({});
   // newTour.save().then(() => {});
-  try {
-    const newTour = await Tour.create(req.body);
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        tour: newTour,
-      },
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e.message,
-    });
-  }
-};
+  const newTour = await Tour.create(req.body);
 
-exports.updateTour = async (req, res) => {
-  try {
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(201).json({
-      status: "success",
-      data: {
-        tour: tour,
-      },
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e.message,
-    });
-  }
-};
+  res.status(201).json({
+    status: "success",
+    data: {
+      tour: newTour,
+    },
+  });
+  // try {
 
-exports.deleteTour = async (req, res) => {
-  try {
-    const deleteTour = await Tour.findByIdAndDelete(req.params.id);
-    res.status(201).json({
-      status: "success",
-      data: {
-        tour: deleteTour,
-      },
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e.message,
-    });
-  }
+  // } catch (e) {
+  //   res.status(400).json({
+  //     status: "fail",
+  //     message: e.message,
+  //   });
+  // }
+});
+
+exports.updateTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(201).json({
+    status: "success",
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const deleteTour = await Tour.findByIdAndDelete(req.params.id);
+  res.status(201).json({
+    status: "success",
+    data: {
+      tour: deleteTour,
+    },
+  });
+
   // res.status(500).json({
   //   status: "error",
   //   message: "This deleteTour handler route is not yet defined",
   // });
-};
+});
 
-exports.getTourStats = async (req, res) => {
-  try {
-    const stats = await Tour.aggregate([
-      {
-        $match: {
-          ratingsAverage: { $gte: 4.5 },
-        },
+exports.getTourStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.aggregate([
+    {
+      $match: {
+        ratingsAverage: { $gte: 4.5 },
       },
-      {
-        $group: {
-          _id: "$duration", // usando null vc ver pra geral, mas usando difficulty, por exemplo, vc ver para os niveis de dificuldade existes cada estatitiscas
-          numTours: { $sum: 1 },
-          sumRatings: { $sum: "$ratingsQuantity" },
-          avgRating: { $avg: "$ratingsAverage" },
-          avgPrice: { $avg: "$price" },
-          minPrice: { $min: "$price" },
-          maxPrice: { $max: "$price" },
-        },
+    },
+    {
+      $group: {
+        _id: "$duration", // usando null vc ver pra geral, mas usando difficulty, por exemplo, vc ver para os niveis de dificuldade existes cada estatitiscas
+        numTours: { $sum: 1 },
+        sumRatings: { $sum: "$ratingsQuantity" },
+        avgRating: { $avg: "$ratingsAverage" },
+        avgPrice: { $avg: "$price" },
+        minPrice: { $min: "$price" },
+        maxPrice: { $max: "$price" },
       },
-      {
-        $match: { _id: { $ne: 10 } }, // aqui diz que não pode com id = diccifulty igual a dez
-      },
-    ]);
+    },
+    {
+      $match: { _id: { $ne: 10 } }, // aqui diz que não pode com id = diccifulty igual a dez
+    },
+  ]);
 
-    res.status(200).json({
-      status: "success",
+  res.status(200).json({
+    status: "success",
 
-      data: {
-        stats,
-      },
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e.message,
-    });
-  }
-};
+    data: {
+      stats,
+    },
+  });
+});
